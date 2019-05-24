94e6f5286b76fc13722e5b7b53f788da5ddf8eeb
kamberovicmubina <mkamberovi1@etf.unsa.ba>
2019-05-25 00:52:29 +0200

    Add project files.

---
+++ EMarket/Models/Market.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMarket.Models
+{
+    public class Market
+    {
+        private List<Proizvod> Proizvodi { get; set; }
+        private List<Proizvod> DostupniProizvodi { get; set; }
+        private List<Osoba> Osobe { get; set; }
+
+        public Market () { }
+        public Market(List<Proizvod> proizvodi, List<Proizvod> dostupniProizvodi, List<Osoba> osobe)
+        {
+            Proizvodi = proizvodi;
+            DostupniProizvodi = dostupniProizvodi;
+            Osobe = osobe;
+        }
+        public void DodajOsobu (Osoba o)
+        {
+            Osobe.Add(o);
+        }
+        public void DodajProizvod (Proizvod p)
+        {
+            Proizvodi.Add(p);
+        }
+    }
+}
---
+++ EMarket/Models/Proizvod.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMarket.Models
+{
+    abstract public class Proizvod
+    {
+        protected String Naziv { get; set; }
+        protected String Proizvodjac { get; set; }
+        protected int KolicinaNaStanju { get; set; }
+        // protected Bitmap SlikaProizvoda { get; set; }
+        protected double Cijena { get; set; }
+        protected bool NaAkciji { get; set; }
+        protected double NovaCijena { get; set; }
+        protected double KolicinaUPakovanju { get; set; }
+        protected static int BrojPutaKupljen { get; set; }
+
+        public Proizvod() { }
+        public Proizvod(String naziv, String proizvodjac, int kolicinaNaStanju, /*Bitmap slikaProizvoda, */ double cijena, bool naAkciji, double novaCijena, double kolicinaUPakovanju)
+        {
+            Naziv = naziv;
+            Proizvodjac = proizvodjac;
+            KolicinaNaStanju = kolicinaNaStanju;
+            //SlikaProizvoda = slikaProizvoda;
+            Cijena = cijena;
+            NaAkciji = naAkciji;
+            NovaCijena = novaCijena;
+            KolicinaUPakovanju = kolicinaUPakovanju;
+            BrojPutaKupljen = 0;
+        }
+    }
+}
